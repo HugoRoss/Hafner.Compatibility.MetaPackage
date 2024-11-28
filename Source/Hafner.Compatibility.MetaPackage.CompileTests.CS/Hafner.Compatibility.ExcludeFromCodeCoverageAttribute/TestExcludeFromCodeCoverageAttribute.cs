6ca8d9a542ed35b53e48d9d195f59c3e902014a3
Christoph Hafner <tibius74@gmail.com>
2024-11-28 23:48:26 +0000

    Package "Hafner.Compatibility.ExcludeFromCodeCoverageAttribute" integrated.

---
+++ Source/Hafner.Compatibility.MetaPackage.CompileTests.CS/Hafner.Compatibility.ExcludeFromCodeCoverageAttribute/TestExcludeFromCodeCoverageAttribute.cs
@@ -0,0 +1,10 @@
+﻿#pragma warning disable CA1812
+
+namespace Hafner.Compatibility.CompileTest.CS;
+
+using System.Diagnostics.CodeAnalysis;
+
+[ExcludeFromCodeCoverage]
+internal sealed class TestExcludeFromCodeCoverageAttribute {
+
+}
